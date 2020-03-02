20e69bae029f952253e5bdb5d7551f7c54d9bdc9
WindCppCoder <35078872+WindCppCoder@users.noreply.github.com>
2020-03-02 17:11:35 -0500

    added key and enabled edit and deletion.

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -9,7 +9,7 @@ type TableState = {
   date: string;
   deleteMode: boolean;
   editMode: boolean;
-  weightEntry: { name: string; weight: number; date: string }[] | null;
+  weightEntry: { name: string; weight: number; date: string ; entryNum: number}[] | null;
 
 };
 
@@ -32,10 +32,7 @@ class Table extends Component<TableProps, TableState> {
       deleteMode: false,
       editMode: false,
       weightEntry: [
-        { name: "Fatty", weight: 463, date: "2020-03-01" },
-        { name: "Slim", weight: 130, date: "2019-09-10" },
-        { name: "Anorexic", weight: 30, date: "2018-12-13" },
-        { name: "Average", weight: 148, date: "2020-06-21" }
+        {name: "", weight: 0, date: "", entryNum: 0}
       ],
     };
     this.handleDateChange = this.handleDateChange.bind(this);
@@ -77,7 +74,8 @@ class Table extends Component<TableProps, TableState> {
             history.push({
               name: res.data.value[i].name,
               weight: res.data.value[i].weight,
-              date: res.data.value[i].date
+              date: res.data.value[i].date,
+              entryNum: res.data.value[i].entryNum
             });
           }
           this.setState({
@@ -90,11 +88,9 @@ class Table extends Component<TableProps, TableState> {
       }
     );
   }
-////incomplete function - error 400, bad request
+
   deleteEntry( row: any) {
     const tempDate = new Moment(row.date).props;
-    //const tempDate = new Date(row.date);
-    console.log(tempDate);
     axios({
       method: "delete",
       url: "/records",
@@ -108,13 +104,13 @@ class Table extends Component<TableProps, TableState> {
         
         ID: this.props.id,
         name: this.props.username,
-        //weight and date are needed to specify a single post to delete; figure out how to pass along information of a row on click
         weight: row.weight,
-        date: tempDate
+        date: tempDate,
+        entryNum: row.entryNum
       }
     }).then(
       res => {
-        this.toggleDelete();
+        //this.toggleDelete(event);
         this.getHistory();
       },
       error => {
@@ -142,19 +138,19 @@ class Table extends Component<TableProps, TableState> {
         
         ID: this.props.id,
         name: this.props.username,
-        //weight and date are needed to specify a single post to delete; figure out how to pass along information of a row on click
         weight: row.weight,
-        date: row.date
-      }/*,
+        date: row.date,
+        entryNum: row.entryNum
+      },
       data: {
         ID: this.props.id,
         name: this.props.username,
         weight: this.state.weight,
         date: this.state.date
-      }*/
+      }
     }).then(
       res => {
-        this.toggleEdit();
+        //this.toggleEdit(event);
         this.getHistory();
       },
       error => {
@@ -206,12 +202,13 @@ class Table extends Component<TableProps, TableState> {
     return (
       this.state.weightEntry &&
       this.state.weightEntry.map((weightEntry, index) => {
-        const { name, weight, date } = weightEntry;
+        const { name, weight, date, entryNum } = weightEntry;
         return (
-          <tr key={date} onClick={this.returnRowData.bind(this, {name, weight, date})} >
+          <tr key={entryNum} onClick={this.returnRowData.bind(this, {name, weight, date, entryNum})} >
             <td> {name} </td>
             <td> {weight} </td>
             <td> {date} </td>
+            <td> {entryNum} </td>
           </tr>
         );
       })
@@ -238,14 +235,16 @@ class Table extends Component<TableProps, TableState> {
     }
   }
 
-  toggleDelete(){
+  toggleDelete(event: any){
+    event.preventDefault();
     this.setState({
       deleteMode: !this.state.deleteMode
     });
     
   }
 
-  toggleEdit(){
+  toggleEdit(event: any){
+    event.preventDefault();
     this.setState({
       editMode: !this.state.editMode
     });
@@ -320,6 +319,7 @@ class Table extends Component<TableProps, TableState> {
             <small>
               After clicking Delete Mode, simply click on the row you wish to delete. <br/>
               After clicking Edit Mode, simply type in the corrected information into the date and weight boxes above, then select the row it is meant to edit.
+              Click the same button to cancel the Edit/Delete mode when you are finished.
             </small>
           </form>
         );
